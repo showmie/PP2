0c1575ed53a4c3b13f6afe5c1bc0794d3b271188
Rob <cblueski@gmail.com>
2018-01-16 17:26:32 +0600

    hehe

---
+++ lab1/lab1/lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{   
+    class student
+    {
+        public string name;
+        public int age;
+        public int exp;
+
+        public student()
+        {
+            name = "Rob";
+            age = 22;
+            exp = 0;
+        }
+        public override string ToString()
+        {
+            return name + " " + age + " " + exp;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student a = new student();
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
